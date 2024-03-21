0ebea6895e100c411b2b68ce870d053385a53e8f
= <=>
2024-03-21 10:21:16 +0100

    feat: add Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,18 @@
-import { useState } from 'react';
-import useGetUserInfo from '../hooks/useGetUserInfo';
-
+import { Search as SearchIcon } from 'iconoir-react';
 function Search({ handleSubmit }: { handleSubmit: (e: React.FormEvent) => void }) {
-  const [user, setUser] = useState<string>('ehdlg');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const { username } = Object.fromEntries(new FormData(e.target as HTMLFormElement));
-
-    setUser(username.toString());
-  };
-
-  const userInfo = useGetUserInfo(user);
-
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input type='text' name='username' id='username' placeholder='Search github username...' />
-      <button type='submit'>Search</button>
+    <form className='search-form' onSubmit={(e) => handleSubmit(e)}>
+      <SearchIcon />
+      <input
+        className='search-input'
+        type='text'
+        name='username'
+        id='username'
+        placeholder='Search github username...'
+      />
+      <button className='search-button' type='submit'>
+        Search
+      </button>
     </form>
   );
 }
