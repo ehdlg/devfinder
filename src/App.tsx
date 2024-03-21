9b5784fc8538f8c4a885fa6797920fed7ac2f368
= <=>
2024-03-21 10:20:02 +0100

    feat: add username validation and pass user as prop to UserInfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import Header from './components/Header';
 import { useState } from 'react';
+import { toast } from 'sonner';
 import './App.css';
+import UserInfo from './components/UserInfo';
 import Search from './components/Search';
 
 function App() {
-  const [user, setUser] = useState<string>('ehdlg');
+  const [user, setUser] = useState<string>('octocat');
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     const { username } = Object.fromEntries(new FormData(e.target as HTMLFormElement));
+
+    if (username.toString().length === 0) return toast.error('The username must not be empty');
 
     setUser(username.toString());
   };
@@ -17,7 +22,7 @@ function App() {
     <main>
       <Header />
       <Search handleSubmit={handleSubmit} />
-      <section id='content'>Content</section>
+      <UserInfo user={user} />
     </main>
   );
 }
