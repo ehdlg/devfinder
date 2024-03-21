74c0e040fee3cd9efbc3f051a9cf33c684b8facb
= <=>
2024-03-21 10:20:27 +0100

    feat: add Toaster to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { Toaster } from 'sonner';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
+    <Toaster richColors position='top-right' />
     <App />
   </>
 );
