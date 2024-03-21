8627ea84b300a78d91b4ff4a8e85844bcf859cf9
= <=>
2024-03-21 10:21:57 +0100

    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import { Twitter, Building, Www, Home } from 'iconoir-react';
+
+export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const NOT_AVAILABLE = 'Not Available';
+
+export const METADATA = ['location', 'twitter_username', 'blog', 'company'] as const;
+
+export const USER_FOOTER_INFO = {
+  twitter_username: Twitter,
+  location: Home,
+  blog: Www,
+  company: Building,
+} as const;
