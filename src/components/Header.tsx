5cc103fc56597c404f679f658172861b34f698b6
= <=>
2024-03-22 09:38:14 +0100

    feat: add light/theme components to handle the theme switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../context/themeContext';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+
 function Header() {
+  const { theme, handleTheme } = useContext(ThemeContext);
+
+  const iconProps = { height: 22, widht: 22, onClick: handleTheme, className: 'theme-icon' };
+
+  const IconTheme =
+    theme === 'dark' ? () => <SunIcon {...iconProps} /> : () => <MoonIcon {...iconProps} />;
+
   return (
     <header>
       <h1>devfinder</h1>
-      <span></span>
+      <IconTheme />
     </header>
   );
 }
